dc3bf2568f718d1544368ec3020fa669741bb7b0
JiaomaWHU <jiaomajiaoma@hotmail.com>
2019-11-30 22:58:22 -0600

    finish logistic

--- cappuccino.py
+++ cappuccino.py
@@ -2,13 +2,13 @@
 import os
 import generate_job_config
 import argparse
+from xml.etree.ElementTree import parse
 
 def args_parse():
     parser = argparse.ArgumentParser(description='Cappuccino args parser')
     parser.add_argument('user', help='Jenkins user name')
     parser.add_argument('password', help='Jenkins user password')
     parser.add_argument('port', help='Jenkins running port')
-    parser.add_argument('dir', help='Test code directory, end with slash')
     return parser.parse_args()
 
 def execute_sudo_command(command):
@@ -16,23 +16,48 @@ def execute_sudo_command(command):
     os.system('echo %s|sudo -S %s' % (sudo_password, command))
 
 def build(args):
-    # generate job config at current dir into  `config.xml`
-    job_name, workspace = generate_job_config.generate_config_from_input(args)
-    # job_name = "sample2"
-    # workspace = "/home/chen/workspace/sample2"
-
-    # set Jenkins privilege on given workspace
-    execute_sudo_command("sudo chmod 777 {}".format(workspace))
-
-    # create job
+    # output the banner for Cappuccino
+    banner = open('banner.txt','r')
+    lines = banner.read()
+    print(lines)
+    banner.close()
     prefix = 'sudo java -jar /var/cache/jenkins/war/WEB-INF/jenkins-cli.jar\
             -auth {}:{} -s http://localhost:{}'.format(args.user, args.password, args.port)
-    CREATE_JOB = prefix + ' create-job {} < config.xml'.format(job_name)
-    execute_sudo_command(CREATE_JOB)
+
+    print("=== Enter \"New\" to build a new job, \"List\" to build exisitng jobs ===")
+    flag = raw_input()
+    if flag != 'New':
+        # list jobs
+        print("=== Existing job list: ===")
+        LIST_JOB = prefix + ' list-jobs'
+        execute_sudo_command(LIST_JOB)
+        print("=== Enter the job you want to build: ===")
+        job_name = raw_input()
+    else:
+        # generate job config at current dir into  `config.xml`
+        job_name, workspace = generate_job_config.generate_config_from_input()
+
+        # set Jenkins privilege on given workspace
+        execute_sudo_command("sudo chmod 777 {}".format(workspace))
+
+        # create job
+        CREATE_JOB = prefix + ' create-job {} < config.xml'.format(job_name)
+        execute_sudo_command(CREATE_JOB)
+
+    # get job output path
+    JOB_INFO = prefix + ' get-job {} > getconfig.xml'.format(job_name)
+    execute_sudo_command(JOB_INFO)
+    tree = parse('getconfig.xml')
+    e = tree.find("builders/hudson.tasks.Shell/command")
+    path = e.text.split()[1]
+
 
     # build job
+    print('=== Start building ===')
     BUILD_JOB = prefix + ' build {}'.format(job_name)
     execute_sudo_command(BUILD_JOB)
+    print('=== Output is created in path: \"{}\" ===').format(path)
+    print('=== Finish building, exit ===')
 
 if __name__ == '__main__':
     args = args_parse()
--- generate_job_config.py
+++ generate_job_config.py
@@ -2,48 +2,50 @@
 from xml.etree.ElementTree import parse
 import os
 
-def generate_config_from_input(args):
-    # output the banner for Cappuccino
-    banner = open('banner.txt','r')
-    lines = banner.read()
-    print(lines)
-    banner.close()
-
-    # configuration for testing job
-    print("Enter the name for your testing job: ")
-    job_name = raw_input()
-    print("Use nosetest: y/n?")
-    flag = raw_input()
-    nosetest_activated = True if flag == "y" else False
-    print("Use pylint: y/n?")
-    flag = raw_input()
-    pylint_activated = True if flag == "y" else False
-    print("Enter your github repo URL, empty means using local files")
-    github_url = raw_input()
-    github_flag = True if github_url else False 
-
+def generate_config_from_input():
     # generate xml configuration
-    dir = args.dir
+    print("=== Enter the path for test repo ===")
+    dir = raw_input()
     project_name = os.path.basename(os.path.normpath(dir))
     ENTER_WORKSPACE = "cd {}../".format(dir)
-    NOSETEST = "nosetests --cover-package={} ".format(project_name)
-    # --with-coverage --cover-inclusive --cover-erase 
-    # > {}output 2>&1 dir
+    NOSETEST = "nosetests --cover-package={} --cover-inclusive ".format(project_name)
+    # fixme: problem here, only support cover package here 
     PYLINT = "pylint -f parseable -d I0011,R0801 {} | tee -a {}output".format(project_name, dir)
     raw_instructions = [ENTER_WORKSPACE]
 
     # configuration for testing job
-    print("Enter the name for your job: ")
+    print("=== Enter the github URL, leave empty for local repo ===")
+    github_url = raw_input()
+    github_flag = True if github_url else False 
+
+    print("=== Enter the name for your job: ===")
     job_name = raw_input()
-    print("Include code coverage? y/n")
+
+    print("** Include code coverage? y/n")
     flag = raw_input()
     if flag == "y":
-        NOSETEST += ''
+        NOSETEST += '--with-coverage '
+    print("** Clean previous statistics? y/n")
+    flag = raw_input()
+    if flag == "y":
+        NOSETEST += '--cover-erase '
+    print("** Be more verbose? y/n")
+    flag = raw_input()
+    if flag == "y":
+        NOSETEST += '-v '
+    print("** Traverse through all path entries of a namespace package? y/n")
+    flag = raw_input()
+    if flag == "y":
+        NOSETEST += '--traverse-namespace '
+    print("** Include code style check? y/n")
+    flag = raw_input()
+    if flag == "n":
+        PYLINT = ''
+
+    NOSETEST += '> {}output 2>&1'.format(dir)
     raw_instructions.append(NOSETEST)
     raw_instructions.append(PYLINT)
     instructions = "\n".join(raw_instructions)
-
-    print("Your testing configuration:")
 
     # create xml file
     tree = parse("config_template.xml")
